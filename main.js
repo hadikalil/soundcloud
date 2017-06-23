422b0daa2cb3e9cb349c7de2a00d201e32f84aa3
[Ethan Jarrell] <[ethan.jarrell@gmail.com]>
2017-06-22 22:35:33 -0400

    my final commit

--- main.js
+++ main.js
@@ -15,10 +15,6 @@ console.log(submitButton);
 
 function searchTracks() {
   let searchResults = trackInput.value;
-    // var fetchApi =
-    //     'https://api.soundcloud.com/tracks/?client_id=86b6a66bb2d863f5d64dd8a91cd8de94&q=' +
-    //     searchResults;
-    // console.log(fetchApi);
 
     fetch('https://api.soundcloud.com/tracks/?client_id=86b6a66bb2d863f5d64dd8a91cd8de94&q=' + searchResults).then(function(response) {
         if (response.status != 200) {
@@ -38,38 +34,38 @@ function searchTracks() {
               document.body.appendChild(searchedFor);
                document.getElementById('searched').appendChild(searchedFor);
               searchedFor.className = 'searchedFor';
-              searchedFor.innerHTML += "showing results for" +": "+ searchResults;
+              searchedFor.innerHTML = "Recent Searches" +": "+ searchResults;
 
 
 
+              var clientId = "/?client_id=86b6a66bb2d863f5d64dd8a91cd8de94";
             function renderTracks() {
+
               return `
-              <audio class="trackToPlay"></audio>
               ${track.map(track =>
               `<div class="box">
               <div class="blankImage"></div>
               <div src="${track.stream_url}"></div>
-              <button id="albumBtn" class="albumButton"><img src="${track.artwork_url}"></img>
-              <div id="songTitle" class="title">${track.title}</div></button>
+              <button id="albumBtn" class="albumButton"><img id="${track.stream_url}${clientId}" src="${track.artwork_url}"></img></button>
+              <div id="songTitle" class="title">${track.title}</div>
               </div>`
               )}
               `
-              // let audioPlayer = document.createElement('audio');
-              // let audioButton = document.getElementsByClassName('albumButton');
-              // console.log(audioPlayer);
-              // console.log(audioButton);
-              // audioButton.addEventListener('click', function(){
-              //   document.body.appendChild(audioPlayer);
-              //   document.getElementById('player1').appendChild(audioPlayer);
-              //
-              // })
-
-
-
-            }
+              }
 
             let markup = `${renderTracks()}`;
             document.getElementById('bands').innerHTML = markup;
+
+            var playThis = document.getElementsByClassName('albumButton');
+                  var playInDocument = document.getElementById('bands').addEventListener('click', function (event) {
+                     event.target = playThis;
+                     let playTrack = `<audio src="${event.target.id}" id="audio" controls="controls"></audio>`
+                  
+                     return document.getElementById('playAudioHere').innerHTML = playTrack
+
+
+            })
+
 
         })
     })
